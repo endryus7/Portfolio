42722b9513301dd1c11182c77f91b6a323dcfbf2
Endryus-Montezano <endryusmontezano@live.com>
2026-03-07 23:24:36 -0300

    Modifica o index e CSS
    Modifica o curriculo, adiciona projetos, modifica o header e css, remove hamburger menu do js

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-let show = true
-
-const menucontent = document.querySelector('.header')
-const menuhamburger = menucontent.querySelector('.mobile_menu')
-
-menuhamburger.addEventListener('click', () => {
-    menucontent.classList.toggle('on', show)
-    show =! show
-})
